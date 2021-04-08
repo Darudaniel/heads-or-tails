bf5359786ce8397b20bba3a17bdac72b2d5cfb8a
darudaniel <danielelrincon@gmail.com>
2021-04-07 19:39:59 -0500

    Animation upgraded

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import '../assets/styles/Loader.css'
+import {Animated} from "react-animated-css";
+
 
 const Loader = () => (
   <div className="loader">
-    <div className="lds-circle"><div className="loader-child"></div></div>
+    <Animated animationIn="rotateIn" animationInDuration="600" isVisible={true}>
+      <div className="lds-circle">
+        <div className="loader-child" >
+
+        </div>
+      </div>
+    </Animated>
   </div>
+  
 
 )
 
